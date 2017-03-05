d29fc08382386256710c3ab5a265b7daa1e671ab
alpsark <alpshrk@gmail.com>
2017-03-06 01:42:04 +0300

    no message

--- FULL_ADDER.v
+++ FULL_ADDER.v
@@ -27,11 +27,11 @@ module FULL_ADDER(
     );
 	wire P , i1 , G  ;
 	XOR x1(A,B,P);
-	NAND n2(A,B,G);	
+	NAND n1(A,B,G);	
 	
 	XOR x2(P,Ci,S);
-	NAND n1(P,Ci,i1);
+	NAND n2(P,Ci,i1);
 	
-	NAND n2(G,i1,Co);
+	NAND n3(G,i1,Co);
 	
 endmodule
