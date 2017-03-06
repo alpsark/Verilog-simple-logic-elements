5dd9fca46446170505a6f9f00f7a2746547039e9
alpsark <alpshrk@gmail.com>
2017-03-06 19:57:16 +0300

    no message

--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/Projects/XilinxISE/HW1/Homework1/testDlatch_NOR_isim_beh.exe" -prj "D:/Projects/XilinxISE/HW1/Homework1/testDlatch_NOR_beh.prj" "work.testDlatch_NOR" "work.glbl" 
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/Projects/GitHub/Verilog-simple-logic-elements/testMUX4_UDP_isim_beh.exe" -prj "D:/Projects/GitHub/Verilog-simple-logic-elements/testMUX4_UDP_beh.prj" "work.testMUX4_UDP" "work.glbl" 
